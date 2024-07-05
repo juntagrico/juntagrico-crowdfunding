42f3220f3e52500ec8860eb8c2156a9fcb423e4a
pascalfree <pascal.free@ihdg.ch>
2024-07-05 19:54:14 +0200

    upgrade to 1.6

--- testurls.py
+++ testurls.py
@@ -6,8 +6,7 @@ from django.contrib import admin
 import juntagrico
 
 urlpatterns = [
-    path(r'admin', admin.site.urls),
-    path(r'', include('juntagrico.urls')),
-    path(r'', include('juntagrico_crowdfunding.urls')),
-    path(r'', juntagrico.views.home),
+    path('admin', admin.site.urls),
+    path('', include('juntagrico.urls')),
+    path('', include('juntagrico_crowdfunding.urls')),
 ]
